248ddedacf424b456dae104d47ff4d89d6dd5b70
Geir Baekholt <geir@baekholt.com>
2005-03-08 14:45:25 +0000

    split interfaces. first step towards two separate tools
    svn path=/CSSRegistry/trunk/; revision=6034

--- interfaces.py
+++ interfaces.py
@@ -8,43 +8,42 @@ class ICSSRegistry(Interface):
         
     def unregisterStyleheet(id ):
         """unregister a registered stylesheet"""
-    
-    def registerScript(id, expression='', contenttype='text/javascript', inline=0, enabled=1):
-        """ register a script"""
             
-    def unregisterScript(id):
-        """ unregister e registered script"""
-        
     def getEvaluatedStyleheets(context):
         """ 
         get the evaluated Stylesheet links and inline styles 
         appropriate to the context for rendering
         """
         
-    def getEvaluatedScripts(context):
-        """
-        get the evaluated Stylesheet links and inline styles 
-        appropriate to the context for rendering
-        """
-        
     def getStylesheets():
         """ get the styleseet objects. For us in management screens"""
-        
-    def getScripts():
-        """ get the scripts. For us in management screens"""
-        
+                
     def manage_saveStylesheet(data):
         """ 
         save stylesheet data from form submission
         data should be a list or tuple of records or dictionaries
         """
         
+        
+class IJSRegistry(Interface):
+    
+    def registerScript(id, expression='', contenttype='text/javascript', inline=0, enabled=1):
+        """ register a script"""
+            
+    def unregisterScript(id):
+        """ unregister e registered script"""
+            
+    def getEvaluatedScripts(context):
+        """
+        get the evaluated Stylesheet links and inline styles 
+        appropriate to the context for rendering
+        """
+
+    def getScripts():
+        """ get the scripts. For us in management screens"""
+
     def manage_saveScripts(data):
         """"
         save script data from form submission
         data should be a list or tuple of records or dictionaries
         """
-        
-
-    
-    

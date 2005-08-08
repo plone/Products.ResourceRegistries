23564e4c1744b683ba80a4d953653cd70f6b0441
Geoff Davis <geoff@geoffdavis.net>
2005-08-08 21:20:14 +0000

    Fixed setting of if-modified-since headers.  Tests still don't fail properly.
    svn path=/ResourceRegistries/trunk/; revision=7836

--- tests/testHTTPHeaders.py
+++ tests/testHTTPHeaders.py
@@ -40,20 +40,36 @@ class TestHTTPHeaders(CSSRegistryTestCase.CSSRegistryTestCase):
         self.assertEqual(response.getHeader('Content-Type'), 'text/css')
         self.assertEqual(response.getStatus(), 200)
 
-    def testIfModifiedSinceHeaders(self):
+    def testIfModifiedSinceHeaders1(self):
         # test with if-modified-since headers early and late (since client clock won't necessarily be synchronized)
         
         # Test that the main page retains its content-type
         self.setRoles(['Manager'])
-        self.portal.REQUEST.set('If-Modified-Since', rfc1123_date((DateTime() - 60.0/(24.0*3600.0)).timeTime())) # if modified in the last minute
+        #self.portal.REQUEST.set('If-Modified-Since', rfc1123_date((DateTime() - 60.0/(24.0*3600.0)).timeTime())) # if modified in the last minute
+        request = self.portal.REQUEST
+        request.environ['IF_MODIFIED_SINCE'] = rfc1123_date((DateTime() - 60.0/(24.0*3600.0))) # if modified in the last minute
+        print '###################################'
+        print 'if-modified-since: ', request.get_header('If-Modified-Since', None)
+        assert request.get_header('If-Modified-Since')
         self.portal.addDTMLMethod('testmethod', file="""/* YES WE ARE RENDERED */""")
         self.tool.registerStylesheet('testmethod')
         response = self.publish(self.toolpath+'/testmethod')
         self.assertEqual(response.getHeader('Content-Type'), 'text/css')
         self.assertEqual(response.getStatus(), 200)
 
+    def testIfModifiedSinceHeaders2(self):
+        # test with if-modified-since headers early and late (since client clock won't necessarily be synchronized)
+
+        # Test that the main page retains its content-type
         self.setRoles(['Manager'])
-        self.portal.REQUEST.set('If-Modified-Since', rfc1123_date((DateTime() + 60.0/(24.0*3600.0)).timeTime())) # if modified in the last minute
+        #self.portal.REQUEST.set('If-Modified-Since', rfc1123_date((DateTime() + 60.0/(24.0*3600.0)).timeTime())) # if modified in the last minute
+        request = self.portal.REQUEST
+        #print request.environ
+        #request.get_header('IF_MODIFIED_SINCE', None)
+        request.environ['IF_MODIFIED_SINCE'] = rfc1123_date((DateTime() + 60.0/(24.0*3600.0))) # if modified in the last minute
+        print '###################################'
+        print 'if-modified-since: ', request.get_header('If-Modified-Since', None)
+        assert request.get_header('If-Modified-Since')
         self.portal.addDTMLMethod('testmethod2', file="""/* YES WE ARE RENDERED */""")
         self.tool.registerStylesheet('testmethod2')
         response = self.publish(self.toolpath+'/testmethod2')

02a6e87234734ced69ab5655e904a2453a693106
Hanno Schlichting <hanno@hannosch.eu>
2009-02-17 22:24:45 +0000

    Handle a missing portal_kss tool gracefully in the kineticstylesheets viewlet.
    svn path=/ResourceRegistries/trunk/; revision=25106

--- Products/ResourceRegistries/browser/kss.py
+++ Products/ResourceRegistries/browser/kss.py
@@ -4,24 +4,26 @@ from Products.CMFCore.utils import getToolByName
 from Products.Five.browser import BrowserView
 from Products.PythonScripts.standard import url_quote
 
-
 class KSSView(BrowserView):
     """ Information for kss rendering. """
 
     def registry(self):
-        return getToolByName(aq_inner(self.context), 'portal_kss')
+        return getToolByName(aq_inner(self.context), 'portal_kss', None)
 
     def skinname(self):
         return aq_inner(self.context).getCurrentSkinName()
 
     def kineticstylesheets(self):
+        result = []
         registry = self.registry()
+        if registry is None:
+            return result
+
         registry_url = registry.absolute_url()
         context = aq_inner(self.context)
 
         kineticstylesheets = registry.getEvaluatedResources(context)
         skinname = url_quote(self.skinname())
-        result = []
         for kss in kineticstylesheets:
             if kss.isExternalResource():
                 src = "%s" % (kss.getId(),)

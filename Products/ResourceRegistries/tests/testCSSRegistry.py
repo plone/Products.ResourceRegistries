adc2e583c73ee80942f7f11bebe0201ccba0bf7c
David Glick <davidglick@groundwire.org>
2009-07-09 07:12:24 +0000

    match the adjusted default
    svn path=/ResourceRegistries/trunk/; revision=27911

--- Products/ResourceRegistries/tests/testCSSRegistry.py
+++ Products/ResourceRegistries/tests/testCSSRegistry.py
@@ -79,7 +79,7 @@ class TestStylesheetRegistration(RegistryTestCase):
         self.assertEqual(self.tool.getResources()[0].getMedia(), None)
         self.assertEqual(self.tool.getResources()[0].getRel(), 'stylesheet')
         self.assertEqual(self.tool.getResources()[0].getTitle(), None)
-        self.assertEqual(self.tool.getResources()[0].getRendering(), 'import')
+        self.assertEqual(self.tool.getResources()[0].getRendering(), 'link')
         self.failUnless(self.tool.getResources()[0].getEnabled())
         
     def testExternalDefaultStylesheetAttributes(self):

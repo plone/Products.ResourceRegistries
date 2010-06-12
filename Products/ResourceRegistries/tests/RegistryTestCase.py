08d5e36d00eec028f10a5dd8fff22b6178ae25d5
Hanno Schlichting <hanno@hannosch.eu>
2010-06-12 14:08:17 +0000

    Avoid deprecation warnings under Zope 2.13.
    svn path=/ResourceRegistries/trunk/; revision=37167

--- Products/ResourceRegistries/tests/RegistryTestCase.py
+++ Products/ResourceRegistries/tests/RegistryTestCase.py
@@ -1,15 +1,21 @@
 from Products.PloneTestCase import PloneTestCase
 from Products.PloneTestCase.PloneTestCase import FunctionalTestCase
 from Products.PloneTestCase.layer import onsetup
-from Products.Five.zcml import load_config
-from Products.Five import fiveconfigure
+
+# BBB Zope 2.12
+try:
+    from Zope2.App.zcml import load_config
+    from OFS import metaconfigure
+except ImportError:
+    from Products.Five.zcml import load_config
+    from Products.Five import fiveconfigure as metaconfigure
 
 @onsetup
 def setupPackage():
-    fiveconfigure.debug_mode = True
+    metaconfigure.debug_mode = True
     import Products.ResourceRegistries.tests
     load_config('test.zcml', Products.ResourceRegistries.tests)
-    fiveconfigure.debug_mode = False
+    metaconfigure.debug_mode = False
 
 setupPackage()
 

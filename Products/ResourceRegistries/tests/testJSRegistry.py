da31f9a01863a28a3dea70f3ff0defe933f4be26
datakurre <asko.soukka@iki.fi>
2011-12-16 11:14:19 +0200

    Added support and tests for OFS.Image.File with Pdata-data.

--- Products/ResourceRegistries/tests/testJSRegistry.py
+++ Products/ResourceRegistries/tests/testJSRegistry.py
@@ -5,6 +5,7 @@ from zope.component import getMultiAdapter
 from zope.contentprovider.interfaces import IContentProvider
 
 from App.Common import rfc1123_date
+from OFS.Image import Pdata
 from DateTime import DateTime
 from AccessControl import Unauthorized
 from zope.interface.verify import verifyObject
@@ -1023,6 +1024,29 @@ class TestCachingHeaders(FunctionalRegistryTestCase):
         self.assertEqual(response.getHeader('Cache-Control'), 'max-age=%d' % int(days*24*3600))
 
 
+class TestPdataAwareness(FunctionalRegistryTestCase):
+
+    def afterSetUp(self):
+        self.tool = getattr(self.portal, JSTOOLNAME)
+        self.tool.clearResources()
+        self.toolpath = '/' + self.tool.absolute_url(1)
+
+        self.setRoles(['Manager'])
+        self.skinstool = getattr(self.portal, 'portal_skins')
+        self.skinstool.custom.manage_addFile(id='hello_world.js',
+                                   content_type='application/javascript',
+                                   file=Pdata('alert("Hello world!");'))
+        self.tool.registerScript('hello_world.js')
+        self.setRoles(['Member'])
+
+    def testPublishFileResourceWithPdata(self):
+        default_skin_name = self.skinstool.getDefaultSkin()
+        response = self.publish(self.toolpath + '/' + default_skin_name +\
+                                '/hello_world.js')
+        self.assertEqual(response.getStatus(), 200)
+        self.failUnless('Hello world!' in str(response))
+
+
 def test_suite():
     from unittest import TestSuite, makeSuite
     suite = TestSuite()
@@ -1046,5 +1070,6 @@ def test_suite():
     suite.addTest(makeSuite(TestResourcePermissions))
     suite.addTest(makeSuite(TestUnicodeAwareness))
     suite.addTest(makeSuite(TestCachingHeaders))
+    suite.addTest(makeSuite(TestPdataAwareness))
 
     return suite
